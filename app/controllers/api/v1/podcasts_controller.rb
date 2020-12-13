0e9fed1c2dd25c85a3f89886ed09e901af480c1c
Chris Kay <ckay84@protonmail.com>
2020-12-13 18:24:52 -0500

    Add podcast_controller

---
+++ app/controllers/api/v1/podcasts_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::PodcastsController < ApplicationController
+
+    def index
+        podcasts = Podcast.all
+    end
+
+    private
+
+    def podcast_params
+      params.require(:podcast).permit(:name)
+    end   
+
+end
