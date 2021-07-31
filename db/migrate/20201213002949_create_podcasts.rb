0df3328638a633b0c59472628303c1ac7582b3ea
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-07-31 18:19:57 -0400

    Delete migrations and clear schema to start fresh

--- db/migrate/20201213002949_create_podcasts.rb
+++
@@ -1,9 +0,0 @@
-class CreatePodcasts < ActiveRecord::Migration[6.0]
-  def change
-    create_table :podcasts do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_045516) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "episodes", force: :cascade do |t|
-    t.string "title"
-    t.integer "podcast_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["podcast_id"], name: "index_episodes_on_podcast_id"
-  end
-
-  create_table "podcasts", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "episodes", "podcasts"
 end
