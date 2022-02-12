1751fa285c21ef121df423fd9cbb180efdb3b1b3
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-02-11 23:22:36 -0500

    Add strong params and create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,18 @@
 class Api::V1::UsersController < ApplicationController
-    
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: {user: user}, status: :ok
+    else
+      render json: { message: 'Failed to create user account' }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
