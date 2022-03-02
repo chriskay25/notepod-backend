b1057eee8d4ae8b2d26c7c6d36b14f73e85dadc8
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-01 23:59:54 -0500

    Add functionality to fetch podcasts by genre

--- app/services/api_query.rb
+++ app/services/api_query.rb
@@ -1,19 +1,23 @@
 require 'podcast_api'
 
 class ApiQuery < ApplicationService
-    attr_reader :type, :offset, :genre_ids
+    attr_reader :type, :offset, :genre_id
 
-    def initialize(type='', offset=0, genre_ids='')
+    def initialize(type: '', offset: 0, genre_id: '')
         @type = type
         @offset = offset
-        @genre_ids = genre_ids
+        @genre_id = genre_id
     end
 
     def call()
+        # client = PodcastApi::Client.new(api_key: ENV['LISTEN_API_KEY']) # nil key returns fake info
         client = PodcastApi::Client.new(api_key: nil) # nil key returns fake info
         case @type
         when 'popular'
-            response = client.fetch_best_podcasts()
+            response = client.fetch_best_podcasts(genre_id: @genre_id)
+        when 'genres'
+            # top_level_only limits the genres to the most popular
+            response = client.fetch_podcast_genres(top_level_only: 1)
         when 'curated'
             response = client.fetch_curated_podcasts_lists()
         end
