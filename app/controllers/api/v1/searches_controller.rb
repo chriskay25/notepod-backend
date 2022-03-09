778be5aee93089815ed042d73808689f47ea5b3b
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-08 19:12:22 -0500

    Edit params to be more specific

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::SearchesController < ApplicationController
 
   def index
-    search_results = ApiQuery.call(params[:type])
+    search_results = ApiQuery.call(type: params[:type], genre_id: params[:genre_id])
     render json: search_results, status: 200
   end
 
   private
 
   def search_params
-    params.require(:search).permit(:type, :offset, :genre_ids)
+    params.require(:search).permit(:type, :offset, :genre_id)
   end
 
 end
