1da11af500b5699dd746b1fa86654fb2483d7f23
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-12 23:32:50 -0500

    Add podcast_id param

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::SearchesController < ApplicationController
 
   def index
-    search_results = ApiQuery.call(type: params[:type], genre_id: params[:genre_id])
+    search_results = ApiQuery.call(type: params[:type], genre_id: params[:genre_id], podcast_id: params[:podcast_id])
     render json: search_results, status: 200
   end
 
   private
 
   def search_params
-    params.require(:search).permit(:type, :offset, :genre_id)
+    params.require(:search).permit(:type, :offset, :genre_id, :podcast_id)
   end
 
 end
