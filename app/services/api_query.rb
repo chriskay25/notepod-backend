b639c95052ed50e4219bb7441ddf6b9d8b722fe8
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-12 23:38:41 -0500

    Add podcast_id attribute and ability to fetch a podcast by id

--- app/services/api_query.rb
+++ app/services/api_query.rb
@@ -1,16 +1,17 @@
 require 'podcast_api'
 
 class ApiQuery < ApplicationService
-    attr_reader :type, :offset, :genre_id
+    attr_reader :type, :offset, :genre_id, :podcast_id
 
-    def initialize(type: '', offset: 0, genre_id: '')
+    def initialize(type: '', offset: 0, genre_id: '', podcast_id: '')
         @type = type
         @offset = offset
         @genre_id = genre_id
+        @podcast_id = podcast_id
     end
 
     def call()
-        # client = PodcastApi::Client.new(api_key: ENV['LISTEN_API_KEY']) # nil key returns fake info
+        # client = PodcastApi::Client.new(api_key: ENV['LISTEN_API_KEY'])
         client = PodcastApi::Client.new(api_key: nil) # nil key returns fake info
         case @type
         when 'popular'
@@ -20,6 +21,8 @@ class ApiQuery < ApplicationService
             response = client.fetch_podcast_genres(top_level_only: 1)
         when 'curated'
             response = client.fetch_curated_podcasts_lists()
+        when 'podcast'
+            response = client.fetch_podcast_by_id(id: @podcast_id)
         end
         response.body
     end
