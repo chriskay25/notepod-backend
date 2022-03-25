cf105c3fdc9b1be516fd13ffdcc3100b56c483e3
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-25 02:11:01 -0400

    Create and run migration for Note

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_224811) do
+ActiveRecord::Schema.define(version: 2022_03_25_011636) do
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.string "podcast_api_id"
+    t.string "episode_api_id"
+    t.string "episode_timestamp"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_07_31_224811) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "notes", "users"
 end
