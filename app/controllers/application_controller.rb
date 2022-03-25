ee74a9e30b956af6a79b34eaff7f1dbdd773777c
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-25 12:10:16 -0400

    Add helper methods for encoding/decoding JWTs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+
+  def encode_token(payload)
+    JWT.encode(payload, 'token', 'HS256')
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decode_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'token', true, { algorithm: 'HS256' })
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
 end
