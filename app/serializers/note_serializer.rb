57be544dc73fb3f1db012b7e43ac7c72814ebd0d
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-01 15:59:40 -0400

    Set key transforms and include jwt in user serializer

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,5 +1,6 @@
 class NoteSerializer
   include JSONAPI::Serializer
+  set_key_transform :camel_lower
   belongs_to :user
   attributes :content, :podcast_id, :episode_id, :episode_timestamp
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,11 @@
 class UserSerializer
   include JSONAPI::Serializer
+  set_key_transform :camel_lower
   has_many :notes
   attributes :username
+
+  attribute :user_jwt do |object, params|
+    params[:jwt]
+  end
+
 end
