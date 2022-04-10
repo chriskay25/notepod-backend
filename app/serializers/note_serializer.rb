6eefc109683538fc03b076e239c9c8ac9889a399
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-04-10 15:57:55 -0400

    Fix incorrect attributes

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -2,5 +2,5 @@ class NoteSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
   belongs_to :user
-  attributes :content, :podcast_id, :episode_id, :episode_timestamp
+  attributes :content, :podcast_api_id, :episode_api_id, :episode_timestamp
 end
